7fa488568609be398a5859666452e8f3948106a0
Enrique Sanchez <esanchez8k@github.com>
2023-02-06 19:40:13 -0500

    created stylized flight table with filler data

---
+++ Develop/assets/script.js
@@ -0,0 +1,64 @@
+const tableBody = document.getElementById("table-body")
+
+let flights = [
+  {
+    time: "08:11",
+    destination: "OMAN",
+    flight: "OX 203",
+    gate: "A 01",
+    remarks: "ON TIME"
+    },
+  {
+    time: "12:39",
+    destination: "LONDON",
+    flight: "CL 320",
+    gate: "C 31",
+    remarks: "CANCELLED"
+    },
+  {
+    time: "13:21",
+    destination: "DUBAI",
+    flight: "DXB 201",
+    gate: "A 19",
+    remarks: "CANCELLED"
+  },
+  {
+    time: "14:01",
+    destination: "FRANKFURT",
+    flight: "FR 402",
+    gate: "B 02",
+    remarks: "ON TIME"
+  },
+  {
+    time: "15:22",
+    destination: "TOKYO",
+    flight: "TK 211",
+    gate: "A 32",
+    remarks: "DELAYED"
+  }
+]
+
+function populateTable () {
+  for (const flight of flights) {
+    const tableRow = document.createElement("tr")
+
+    for( const flightDetail in flight) {
+      const tableCell = document.createElement("td")
+      const word = Array.from(flight[flightDetail])
+      for (const letter of word) {
+        const letterElement = document.createElement('div')
+        letterElement.classList.add('flip')
+        letterElement.textContent = letter
+        tableCell.append(letterElement)
+      }
+      tableRow.append(tableCell)
+    }
+
+    tableBody.append(tableRow)
+
+  }
+}
+
+populateTable()
+
+ 
