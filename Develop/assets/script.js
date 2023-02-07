777e6c09a2f6074c3ff7dc51be3a3c425034b173
Enrique Sanchez <esanchez8k@github.com>
2023-02-06 20:02:25 -0500

    finished stylized/animated flight table with filler data, will incorporate API data next

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -45,20 +45,21 @@ function populateTable () {
     for( const flightDetail in flight) {
       const tableCell = document.createElement("td")
       const word = Array.from(flight[flightDetail])
-      for (const letter of word) {
+
+      for (const [index, letter] of word.entries()) {
         const letterElement = document.createElement('div')
+
+        setTimeout(() => {
         letterElement.classList.add('flip')
         letterElement.textContent = letter
         tableCell.append(letterElement)
+        }, 100 * index)
+
       }
       tableRow.append(tableCell)
     }
-
     tableBody.append(tableRow)
-
   }
 }
 
 populateTable()
-
- 
