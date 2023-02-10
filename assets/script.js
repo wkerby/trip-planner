015d1ebd579100a3673b3590549d9eebdd5efd1a
Enrique Sanchez <esanchez8k@github.com>
2023-02-09 21:38:35 -0500

    added attractions box and populated with attractions li/ cleaned up unused code

--- assets/script.js
+++ assets/script.js
@@ -37,74 +37,6 @@ function hideButton(){
 fetchButton.addEventListener('click', getApi)
 
 
-//----------------------------------Departing Flights Table------------------------------------------------------
-
-const attractionsList = document.getElementById("attractions-list")
-
-// let flights = [
-//   {
-//     time: "08:11",
-//     destination: "OMAN",
-//     flight: "OX 203",
-//     gate: "A 01",
-//     remarks: "ON TIME"
-//   },
-//   {
-//     time: "12:39",
-//     destination: "LONDON",
-//     flight: "CL 320",
-//     gate: "C 31",
-//     remarks: "CANCELLED"
-//   },
-//   {
-//     time: "13:21",
-//     destination: "DUBAI",
-//     flight: "DXB 201",
-//     gate: "A 19",
-//     remarks: "CANCELLED"
-//   },
-//   {
-//     time: "14:01",
-//     destination: "FRANKFURT",
-//     flight: "FR 402",
-//     gate: "B 02",
-//     remarks: "ON TIME"
-//   },
-//   {
-//     time: "15:22",
-//     destination: "TOKYO",
-//     flight: "TK 211",
-//     gate: "A 32",
-//     remarks: "DELAYED"
-//   }
-// ]
-
-// function populateTable() {
-//   for (const flight of flights) {
-//     const tableRow = document.createElement("tr")
-
-//     for (const flightDetail in flight) {
-//       const tableCell = document.createElement("td")
-//       const word = Array.from(flight[flightDetail])
-
-//       for (const [index, letter] of word.entries()) {
-//         const letterElement = document.createElement('div')
-
-//         setTimeout(() => {
-//           letterElement.classList.add('flip')
-//           letterElement.textContent = letter
-//           tableCell.append(letterElement)
-//         }, 100 * index)
-
-//       }
-//       tableRow.append(tableCell)
-//     }
-//     tableBody.append(tableRow)
-//   }
-// }
-
-// populateTable()
-
 document.addEventListener('DOMContentLoaded', () => {
   // Functions to open and close a modal
   function openModal($el) {
@@ -150,14 +82,13 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-const attractions = []
+// const attractions = []
 
 //create function that fetches city ID based off of city input
 function getAttractions(city) {
   // var city = document.querySelector("option").textContent;
-  // var boxOrderListEl = documnent.querySelector(".attractions");
-  console.log(ulAttractionsEl);
   var fetchUrl = "https://travel-advisor.p.rapidapi.com/locations/search?query=" + city + "&limit=30&offset=0&units=km&location_id=1&currency=USD&sort=relevance&lang=en_US"
+  var ulAttractionsEl = document.querySelector('#attractions-list');
 
   var apiOptions = {
     method: 'GET',
@@ -175,33 +106,22 @@ function getAttractions(city) {
     .then(function (data) {
       for (var i = 0; i < data.data.length; i++) {
         if (data.data[i].result_type === "things_to_do") {
-          var ulAttractionsEl = document.querySelector('#attractions-list');
+          console.log(data.data[i].result_object);
+          console.log(data.data[i].result_object.name);
           var attraction = data.data[i].result_object.name;
           var newLi = document.createElement("li");
+          newLi.setAttribute('class', 'attraction');
           newLi.textContent = attraction;
           ulAttractionsEl.appendChild(newLi);
-
+  
         }
       };
+
     });
 
-          // for (const [index, letter] of word.entries()) {
-          //   const letterElement = document.createElement('div')
+}
 
-          //   setTimeout(() => {
-          //     letterElement.classList.add('flip')
-          //     letterElement.textContent = letter
-          //     tableCell.append(letterElement)
-          //   }, 100 * index)
-
-          // }
-          attractionsList.append(newEntry)
-        }
-      // tableBody.append(tableRow)
-
-
-getAttractions("New York");
-
+getAttractions("pattaya");
 
 
 
